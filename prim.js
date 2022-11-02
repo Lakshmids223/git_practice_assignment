dd1486a51ee343732537ad27eab26c5bf0481a29
Lakshmids223 <dslakshmi223@gmail.com>
2022-11-02 11:46:26 +0530

    added prime and palindrome files

---
+++ prim.js
@@ -0,0 +1,13 @@
+//prime number or not
+function prime(n)
+let count=0;
+for(let i=0;i<=n;i++){
+    if(n%i==0){
+        count++;
+    }
+}
+if(count==2){
+    console.log("Yes");
+}else{
+    console.log("No");
+}
